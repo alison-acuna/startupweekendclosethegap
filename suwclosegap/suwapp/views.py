5320e13fab05287f7431c9a0b3699434896d0736
alison-acuna <aacuna136@gmail.com>
2016-05-21 18:44:53 -0700

    nav pages done

--- suwclosegap/suwapp/views.py
+++ suwclosegap/suwapp/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def home(request):
+    return render(request, 'suwapp/home.html', {})
+
+def ranked(request):
+    return render(request, 'suwapp/ranked.html', {})
+
+def compensation(request):
+    return render(request, 'suwapp/compensation.html', {})        
